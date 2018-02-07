ef399b34fbdd82f516a286a1d48a90e38a571c19
Dominick Del Bosque <dominick@theosfp.com>
2018-02-06 20:04:17 -0800

    Contact Page Layout.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,8 +12,8 @@ class Contact extends Component {
 					</Column>
 				</Row>
 					<Flex wrap mx={5}>
-						<Box w={[1,1/2]} px={1} pt={3}>
-							<Text f={[5, 10, 15]}>
+						<Box w={[1, 1/2]} px={1} pt={3}>
+							<Text>
 							Tired of staring at the blank page and blinking cursor? We've all
 							been there, and we understand it can be overwhelming.
 							Let's brainstorm about your vision, goals, and what we can do to 
@@ -21,8 +21,8 @@ class Contact extends Component {
 							</Text>
 						</Box>
 						
-						<Box w={[1,1/2]} px={1}>
-							<Text f={[5, 10, 15]}>
+						<Box w={[1, 1/2]} px={1}>
+							<Text>
 							<h3>(650) 524-5197</h3>
 							<h3>innovation@createoddity.com</h3>
 							</Text>
