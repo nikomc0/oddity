ca1efdd6fb1175f7911c114e001b620189d90b8f
Dominick Del Bosque <dominick@theosfp.com>
2018-02-06 19:55:08 -0800

    Modified Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,8 +12,8 @@ class Contact extends Component {
 					</Column>
 				</Row>
 					<Flex wrap mx={5}>
-						<Box w={[1,1/2]} px={1} pt={4}>
-							<Measure f={[10, 15, 20]}>
+						<Box w={[1,1/2]} px={1} pt={3}>
+							<Measure f={[5, 10, 15]}>
 							Tired of staring at the blank page and blinking cursor? We've all
 							been there, and we understand it can be overwhelming.
 							Let's brainstorm about your vision, goals, and what we can do to 
@@ -22,7 +22,7 @@ class Contact extends Component {
 						</Box>
 						
 						<Box w={[1,1/2]} px={1}>
-							<Text f={[10, 15, 20]}>
+							<Text f={[5, 10, 15]}>
 							<h3>(650) 524-5197</h3>
 							<h3>innovation@createoddity.com</h3>
 							</Text>
